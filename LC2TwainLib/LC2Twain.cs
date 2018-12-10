f2b36c7676eeaf91544a84d21688bed79d740283
David Honisch <david@honisch.org>
2018-12-10 18:33:13 +0100

    LC2TwainLib - Extends .NET Twain Library

---
+++ LC2TwainLib/LC2Twain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwainLib;
+
+namespace LC2TwainLib
+{
+    public class LC2Twain:Twain
+    {
+    }
+}
