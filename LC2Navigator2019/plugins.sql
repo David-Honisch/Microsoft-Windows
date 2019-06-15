b65f2db8b29c34a5a9a921d44715139154930b0d
David Honisch <d.honisch@slenderiser.de>
2019-06-15 19:39:38 +0200

    Update - 19:39:38,41

--- LC2Navigator2019/plugins.sql
+++ LC2Navigator2019/plugins.sql
@@ -20,6 +20,10 @@ SELECT '<h4>INSERT INTO importscripts</h4>';
 INSERT OR REPLACE INTO importscripts (first_name,name,url) 
 values 
 ('.\\resources\\cmd\\updates.bat','.\\resources\\cmd\\updates.bat','.\\resources\\cmd\\updates.bat');
+SELECT '<h4>INSERT INTO importscripts</h4>';
+INSERT OR REPLACE INTO importscripts (first_name,name,url) 
+values 
+('.\\resources\\cmd\\updates.bat','.\\resources\\cmd\\updates.bat','.\\resources\\cmd\\updates.bat');
 --.separator "\t"
 --.import .\\plugins.csv importscripts
 SELECT first_name, COUNT(*) c FROM importscripts GROUP BY first_name HAVING c > 1;
