6d4bd0f5fbabf93ba08224ab4cef84af76aba464
David Honisch <d.honisch@slenderiser.de>
2019-06-15 19:35:34 +0200

    Update - 19:35:34,49

--- LC2Navigator2019/plugins.sql
+++ LC2Navigator2019/plugins.sql
@@ -9,22 +9,17 @@ SELECT '<h4>DELETING DUPLICATES from TABLE</h4>';
 DELETE FROM importscripts 
 where rowid not in (select min(rowid)
                     from importscripts
-                    group by first_name,first_name,url);
+                    group by first_name,name,url);
 SELECT '<h4>DELETING DUPLICATES DONE</h4>';
 SELECT '<h4>DELETING DUPLICATES from application TABLE</h4>';
 DELETE FROM application 
 where rowid not in (select min(rowid)
                     from application
                     group by first_name,first_name,url);
-SELECT '<h4>DELETING DUPLICATES DONE</h4>';
-SELECT '<h4>INSERT INTO application</h4>';
-INSERT INTO application (first_name,name,url) 
-values 
-('importapplications.bat','importapplications.bat','importapplications.bat');
 SELECT '<h4>INSERT INTO importscripts</h4>';
-INSERT INTO importscripts (first_name,name,url) 
+INSERT OR REPLACE INTO importscripts (first_name,name,url) 
 values 
-('importapplications.bat','importapplications.bat','importapplications.bat');
+('.\\resources\\cmd\\updates.bat','.\\resources\\cmd\\updates.bat','.\\resources\\cmd\\updates.bat');
 --.separator "\t"
 --.import .\\plugins.csv importscripts
 SELECT first_name, COUNT(*) c FROM importscripts GROUP BY first_name HAVING c > 1;
