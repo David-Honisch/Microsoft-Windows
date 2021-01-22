e7a9673c2b5a8b894590c3922155d1bc8b50904f
dh <david@honisch.org>
2021-01-22 20:52:31 +0100

    all plugins - apache maven plugin added - used for binary downloads in later releases

--- LC2Navigator2020/plugins/all/resources/sql/all.zip.sql
+++ LC2Navigator2020/plugins/all/resources/sql/all.zip.sql
@@ -1,20 +1,17 @@
-SELECT '<h1>CORE PLUGIN SQL SCRIPT IS RUNNING</h1>';
+SELECT '<h1>ALLPLUGINS PLUGIN SQL SCRIPT IS RUNNING</h1>';
 SELECT '<p>Deleting import script</p>';
 --DELETE FROM importscripts;
 SELECT '<h5>RUNNING IMPORT</h5>';
 SELECT '<p>SET CLEAR</p>';
 SELECT '<h4>DELETING application</h4>';
 --SELECT '<h4>DELETING application</h4>';
---INSERT OR REPLACE INTO importscripts (first_name,name,url) 
---values 
---('Grab Urls v.1.0','Grab Urls v.1.0','.\\resources\\plugins\\atari.bat atariemulator.zip');
 --.separator "\t"
 --.import .\\plugins.csv importscripts
 --SELECT first_name, COUNT(*) c FROM importscripts GROUP BY first_name HAVING c > 1;
 
 SELECT '<h1>UPDATE SQL SCRIPT DONE</h1>';
 
-SELECT '<h3>LC2PHP Plugin SCRIPT DONE</h3>';
+SELECT '<h3>ALLPLUGINS Plugin SCRIPT DONE</h3>';
 INSERT OR REPLACE INTO plugins (first_name,name,url) 
 values 
 ('LC2PHP Downloader v.1.02','LC2PHP Downloader v.1.02','exec .\\resources\\cmd\\getupdates.bat /plugins/lc2php.zip lc2php.zip');
@@ -32,10 +29,11 @@ INSERT OR REPLACE INTO plugins (first_name,name,url)
 values 
 ('MySQLdocker v.1.01a','MySQLdocker Downloader v.1.01a','exec .\\resources\\cmd\\getupdates.bat /plugins/mysqldocker.zip mysqldocker.zip');
 
---INSERT OR REPLACE INTO plugins (first_name,name,url) 
---values 
---('Excel Transformer v.1.01a(IN PROGRESS!!NOT WORKING!!)','AllPlugins Menu Downloader v.1.01a(IN PROGRESS!!NOT WORKING!!)','exec .\\resources\\cmd\\getupdates.bat /plugins/excelimport.zip excelimport.zip');
+INSERT OR REPLACE INTO plugins (first_name,name,url) 
+values 
+('Maven v.3.5.2','Maven Downloader v.3.5.2','exec .\\resources\\cmd\\getupdates.bat /plugins/apache_maven.zip apache_maven.zip');
 
-SELECT '<p>Applications imported'||count(*)||'</p>' from application;
+
+SELECT '<p>All Plugin Downloaders imported'||count(*)||'</p>' from application;
 
 SELECT '<h5>SQL IMPORT DONE</h5>';
