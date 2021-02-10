ddbc0d8eca8ad8d910d1861e3c5ad1a270ee6ddb
dh <david@honisch.org>
2021-02-10 22:45:02 +0100

    createplugin - default sql template - direct import to sqlite database

--- LC2Navigator2020/plugins/_createplugin.bat
+++ LC2Navigator2020/plugins/_createplugin.bat
@@ -3,4 +3,10 @@ set input=_createplugin.csv
 REM set output=org.letztechance.domain.web.GrabUrls.zip
 for /f "usebackq tokens=1-3 delims=;" %%a in ("%input%") do (	
 	mkdir %%a\resources\plugins\%%a
+	mkdir %%a\resources\sql
+	echo creating sql script
+	call resources\default.sql.bat "%cd%\%%a\resources\sql\%%a.zip.sql" %%a
+	echo creating sql script done
 )
+
+timeout 3
