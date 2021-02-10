52bb4f0cddf5d96a1665a2c8fd76ee9d8a35971d
dh <david@honisch.org>
2021-02-11 00:55:09 +0100

    lc2irc update - sample script supporting chat messages, xdcc, ctcp

--- LC2Navigator2020/plugins/resources/default.sql.bat
+++ LC2Navigator2020/plugins/resources/default.sql.bat
@@ -9,12 +9,7 @@ REM echo --DELETE FROM importscripts; >> %out%
 echo SELECT '^<h5^>RUNNING IMPORT^</h5^>'; >> %out%
 echo SELECT '^<h4^>SET CLEAR^</h4^>'; >> %out%
 echo SELECT '^<h4^>DELETING application^</h4^>'; >> %out%
-REM echo --INSERT OR REPLACE INTO importscripts (first_name,name,url)  >> %out%
-REM echo --values  >> %out%
-REM echo --('Grab Urls v.1.0','Grab Urls v.1.0','.\\resources\\plugins\\atari.bat atariemulator.zip'); >> %out%
-REM echo --.separator "\t" >> %out%
-REM echo --.import .\\plugins.csv importscripts >> %out%
-REM echo --SELECT first_name, COUNT(*) c FROM importscripts GROUP BY first_name HAVING c ^> 1; >> %out%
+REM echo --SELECT first_name, COUNT(*) c FROM application GROUP BY first_name HAVING c ^> 1; >> %out%
 echo SELECT '^<h1^>UPDATE %in% SQL SCRIPT DONE^</h1^>'; >> %out%
 echo INSERT INTO application VALUES(25,'%in% v.1.01a','%in% v.1.01a','','','','','exec .\\resources\\plugins\\%in%\\%in%.bat .\\resources\\plugins\\%in%\\menu.csv'); >> %out%
 echo SELECT '^<h5^>SQL %in% IMPORT DONE^</h5^>'; >> %out%
