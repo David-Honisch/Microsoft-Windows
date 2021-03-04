a047505a43d5ad84a0260acfa42115a7ce582676
dh <david@honisch.org>
2021-03-04 21:30:52 +0100

    all update sql fix

--- LC2Navigator2020/plugins/all.cmp.bat
+++ LC2Navigator2020/plugins/all.cmp.bat
@@ -6,17 +6,13 @@ set output=all.zip
 move all.zip all_old.zip
 type nul>%target%
 echo CREATING
-REM echo SELECT 'Count of plugins:'||count(*)||'' from plugins; >> %target%
+echo SELECT 'Plugins count ',count(*) from plugins; >> %target%
 for /f "usebackq tokens=1-3 delims=;" %%a in ("%inputlist%") do (
 	echo CREATING %%a
-	echo SELECT 'Importing %%a Plugin SCRIPT'; >> %target%
-	set "sql=INSERT OR REPLACE INTO plugins(first_name,name,url)values('%%a Download','%%a Download','exec .\\resources\\cmd\\getupdates.bat /plugins/%%a %%a');"
-	set sql
-	call unqoute "%sql%" >> %target%
+	echo SELECT 'Importing %%a Plugin SCRIPT'; >> %target%	
+	call unqoute "INSERT OR REPLACE INTO plugins(first_name,name,url)values('%%a Download','%%a Download','exec .\\resources\\cmd\\getupdates.bat /plugins/%%a %%a');" >> %target%
 	echo SELECT 'Importing %%a Plugin SCRIPT DONE'; >> %target%	
 )
-REM call unqoute "SELECT 'Count of all imported plugins:'||count(*)||'' from plugins;" >> %target%
 echo SELECT 'Plugins count ',count(*) from plugins; >> %target%
 echo SELECT 'Running SQL plugins import DONE'; >> %target%
 call powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%input%', '%output%');"
-REM zip -9 -m -o %output% %input%
