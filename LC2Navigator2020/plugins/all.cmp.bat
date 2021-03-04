98b5cccce959c2fdf6f5cd12431aeccd56262d13
dh <david@honisch.org>
2021-03-04 20:28:28 +0100

    sql all

--- LC2Navigator2020/plugins/all.cmp.bat
+++ LC2Navigator2020/plugins/all.cmp.bat
@@ -7,9 +7,9 @@ move all.zip all_old.zip
 type nul > %target%
 echo "SELECT '<p>Count of plugins:'||count(*)||'</p>' from plugins;" >> %target%
 for /f "usebackq tokens=1-3 delims=;" %%a in ("%inputlist%") do (
-	echo "SELECT '^<h3^>Importing %%a Plugin SCRIPT^</h3>';" >> %target%
+	echo "SELECT '<h3>Importing %%a Plugin SCRIPT</h3>';" >> %target%
 	echo "INSERT OR REPLACE INTO plugins (first_name,name,url) values ('%%a Download','%%a Download','exec .\\resources\\cmd\\getupdates.bat /plugins/%%a %%a');" >> %target%
-	echo "SELECT '^<h3^>Importing %%a Plugin SCRIPT DONE^</h3^>';" >> %target%	
+	echo "SELECT '<h3>Importing %%a Plugin SCRIPT DONE</h3>';" >> %target%	
 )
 echo "SELECT '<p>Count of all imported plugins:'||count(*)||'</p>' from plugins;" >> %target%
 echo "SELECT '<h3>Running SQL plugins import DONE</h3>';" >> %target%
