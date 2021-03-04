6bf32246eb20d635c4e0846a3f620ae130b9061b
dh <david@honisch.org>
2021-03-04 19:44:03 +0100

    createplugin

--- LC2Navigator2020/plugins/_createplugin.bat
+++ LC2Navigator2020/plugins/_createplugin.bat
@@ -3,10 +3,26 @@ set input=_createplugin.csv
 REM set output=org.letztechance.domain.web.GrabUrls.zip
 for /f "usebackq tokens=1-3 delims=;" %%a in ("%input%") do (	
 	mkdir %%a\resources\plugins\%%a
+	mkdir %%a\resources\plugins\%%a\install
+	mkdir %%a\resources\plugins\%%a\menu
 	mkdir %%a\resources\sql
+	
 	echo creating sql script
-	call resources\default.sql.bat "%cd%\%%a\resources\sql\%%a.zip.sql" %%a
-	echo creating sql script done
+	call resources\default.sql.bat "%cd%\%%a\resources\sql\%%a.zip.sql" %%a	
+
+	echo creating default config script	
+	copy resources\config.bat "%cd%\%%a\resources\plugins\%%a\config.bat"
+	call resources\defaultconfig.file.bat "%cd%\%%a\resources\plugins\%%a\config.bat" %%a
+	copy resources\menu.csv "%cd%\%%a\resources\plugins\%%a\menu.csv"
+	copy resources\install\install.csv "%cd%\%%a\resources\plugins\%%a\install\install.csv"
+	copy resources\install.html "%cd%\%%a\resources\plugins\%%a\install.html"
+	copy resources\install.js "%cd%\%%a\resources\plugins\%%a\install.js"
+	
+	echo creating default script	
+	REM call resources\default.file.bat "%cd%\%%a\resources\plugins\%%a\%%a.bat" %%a	
+	copy resources\default.file.bat.txt "%cd%\%%a\resources\plugins\%%a\%%a.bat"
+	
+	
 )
 
 timeout 3
