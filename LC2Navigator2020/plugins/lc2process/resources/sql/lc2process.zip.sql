f39e3bd889fbda5c2a3e554fc5176e642ce04eb7
dh <david@honisch.org>
2021-03-07 00:01:13 +0100

    lc2process update

--- LC2Navigator2020/plugins/lc2process/resources/sql/lc2process.zip.sql
+++ LC2Navigator2020/plugins/lc2process/resources/sql/lc2process.zip.sql
@@ -4,5 +4,5 @@ SELECT '<h5>RUNNING IMPORT</h5>';
 SELECT '<h4>SET CLEAR</h4>'; 
 SELECT '<h4>DELETING application</h4>'; 
 SELECT '<h1>UPDATE lc2process SQL SCRIPT DONE</h1>'; 
-INSERT INTO application VALUES(25,'lc2process v.1.01a','lc2process v.1.01a','','','','','exec .\\resources\\plugins\\lc2process\\lc2process.bat .\\resources\\plugins\\lc2process\\menu.csv'); 
+INSERT INTO application VALUES(5,'lc2process v.1.01a','lc2process v.1.01a','','','','','exec .\\resources\\plugins\\lc2process\\lc2process.bat .\\resources\\plugins\\lc2process\\menu.csv'); 
 SELECT '<h5>SQL lc2process IMPORT DONE</h5>'; 
