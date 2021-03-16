b9e2fb5c0ce8666aeae573f668bda85aca95afd8
dh <david@honisch.org>
2021-03-16 21:34:41 +0100

    sql updates

--- LC2Navigator2020/plugins/resources/default.sql.bat
+++ LC2Navigator2020/plugins/resources/default.sql.bat
@@ -11,5 +11,5 @@ echo SELECT '^<h4^>SET CLEAR^</h4^>'; >> %out%
 echo SELECT '^<h4^>DELETING application^</h4^>'; >> %out%
 REM echo --SELECT first_name, COUNT(*) c FROM application GROUP BY first_name HAVING c ^> 1; >> %out%
 echo SELECT '^<h1^>UPDATE %in% SQL SCRIPT DONE^</h1^>'; >> %out%
-echo INSERT INTO application VALUES(25,'%in% v.1.01a','%in% v.1.01a','','','','','exec .\\resources\\plugins\\%in%\\%in%.bat .\\resources\\plugins\\%in%\\menu.csv'); >> %out%
+echo INSERT OR REPLACE INTO application(person_id, name, first_name, description, zipcode, city, street, url)VALUES(100,'%in% v.1.01a','%in% v.1.01a','','','','','exec .\\resources\\plugins\\%in%\\%in%.bat .\\resources\\plugins\\%in%\\menu.csv'); >> %out%
 echo SELECT '^<h5^>SQL %in% IMPORT DONE^</h5^>'; >> %out%
