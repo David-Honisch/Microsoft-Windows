723d291a4eb181df5d8f6775e260bd7c32fc9c46
dh <david@honisch.org>
2021-04-22 19:09:15 +0200

    Test upload

--- LC2Navigator2020/plugins/LC2LinkCLI.cmp.bat
+++ LC2Navigator2020/plugins/LC2LinkCLI.cmp.bat
@@ -1,11 +1,6 @@
 @echo off
-<<<<<<< HEAD:LCUnityCEFServer.Client/install/createServerinstall.bat
-set "input=D:\src\unity\bin\lcunityserver"
-set output=serverinstall.zip
-=======
 set input=lc2linkcli
 set output=lc2linkcli.zip
->>>>>>> db44eafad704b6a285baa67588ec98b4231d0b40:LC2Navigator2020/plugins/LC2LinkCLI.cmp.bat
-move %output% %output%_old.zip
+move %output% %input%_old.zip
 call powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%input%', '%output%');"
 REM zip -9 -m -o %output% %input%
--- LC2Navigator2020/plugins/mysqldocker.cmp.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set input=mysqldocker
-set output=mysqldocker.zip
-move %output% mysqldocker_old.zip
-call powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%input%', '%output%');"
-REM zip -9 -m -o %output% %input%
