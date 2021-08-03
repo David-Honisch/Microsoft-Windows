6e8ce652fcc02ba0f4f32b6038a68d759064199c
dh <david@honisch.org>
2021-08-03 21:18:49 +0200

    SQL Fix Update

--- LC2Navigator2020/plugins/lc2process/resources/sql/lc2process.zip.sql
+++ LC2Navigator2020/plugins/lc2process/resources/sql/lc2process.zip.sql
@@ -5,7 +5,7 @@ SELECT '<h4>SET CLEAR</h4>';
 SELECT '<h4>DELETING application</h4>'; 
 select count(*) as count from application;
 SELECT '<h1>UPDATE lc2process SQL SCRIPT DONE</h1>'; 
-INSERT OR REPLACE INTO application(person_id, name, first_name, description, zipcode, city, street, url)
-VALUES(8,'lc2process v.1.01a','lc2process v.1.01a','','','','','exec .\\resources\\plugins\\lc2process\\lc2process.bat .\\resources\\plugins\\lc2process\\menu.csv'); 
+INSERT OR REPLACE INTO application(name, first_name, description, zipcode, city, street, url)
+VALUES('lc2process v.1.01a','lc2process v.1.01a','','','','','exec .\\resources\\plugins\\lc2process\\lc2process.bat .\\resources\\plugins\\lc2process\\menu.csv'); 
 select count(*) as count from application;
 SELECT '<h5>SQL lc2process IMPORT DONE</h5>'; 
