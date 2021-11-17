177dbd10d3e9d958c33190e9736d68835b92e06e
dh <david@honisch.org>
2021-11-17 20:00:06 +0100

    default plugin schema importer

---
+++ LC2Navigator2022/plugins/resources/defaultconfig.file.bat
@@ -0,0 +1,9 @@
+@echo off
+set "target=%1"
+set "name=%2"
+echo OUT:%target%
+echo set plugin=%name%>> %target%
+echo set plugin=%name%>> %target%
+echo set title=%name% Plugin>> %target%
+echo set version=v.0.1.121a>> %target%
+@echo on
