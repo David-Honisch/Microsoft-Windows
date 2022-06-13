109716de288aa899547cc88f8e186cc022bef56b
dh <david@honisch.org>
2022-06-13 20:29:19 +0200

    LC2WiFiDirectLegacyAPI

--- LC2Navigator2020/plugins/_createplugin.bat
+++ LC2Navigator2020/plugins/_createplugin.bat
@@ -3,19 +3,27 @@ set input=_createplugin.csv
 REM set output=org.letztechance.domain.web.GrabUrls.zip
 for /f "usebackq tokens=1-3 delims=;" %%a in ("%input%") do (	
 	mkdir %%a\resources\plugins\%%a
-	mkdir %%a\resources\plugins\%%a\install
+	mkdir %%a\resources\plugins\%%a\import
 	mkdir %%a\resources\plugins\%%a\menu
 	mkdir %%a\resources\plugins\%%a\resources
 	mkdir %%a\resources\sql
 	
 	echo creating sql script
 	call resources\default.sql.bat "%cd%\%%a\resources\sql\%%a.zip.sql" %%a	
+	
+	call resources\schema.sql.bat "%cd%\%%a\resources\plugins\%%a\schema.sql" %%a	
 
 	echo creating default config script	
 	copy resources\config.bat "%cd%\%%a\resources\plugins\%%a\config.bat"
 	call resources\defaultconfig.file.bat "%cd%\%%a\resources\plugins\%%a\config.bat" %%a
-	copy resources\menu.csv "%cd%\%%a\resources\plugins\%%a\menu.csv"
-	copy resources\install\install.csv "%cd%\%%a\resources\plugins\%%a\install\install.csv"
+	echo copy resources\menu.csv "%cd%\%%a\resources\plugins\%%a\menu.csv"
+	
+	call resources\menu.bat "%cd%\%%a\resources\plugins\%%a\" %%a
+	@echo off
+	
+	
+
+	copy resources\import\import.csv "%cd%\%%a\resources\plugins\%%a\import\import.csv"
 	copy resources\install.html "%cd%\%%a\resources\plugins\%%a\install.html"
 	copy resources\install.js "%cd%\%%a\resources\plugins\%%a\install.js"
 	copy resources\config.js "%cd%\%%a\resources\plugins\%%a\config.js"
@@ -23,11 +31,16 @@ for /f "usebackq tokens=1-3 delims=;" %%a in ("%input%") do (
 	echo creating default script	
 	REM call resources\default.file.bat "%cd%\%%a\resources\plugins\%%a\%%a.bat" %%a	
 	copy resources\default.file.bat.txt "%cd%\%%a\resources\plugins\%%a\%%a.bat"
-	copy resources\default.file.bat.txt "%cd%\%%a\resources\plugins\%%a\index.bat"
+	copy resources\index.bat "%cd%\%%a\resources\plugins\%%a\index.bat"
 	
 	copy resources\resources\index.bat "%cd%\%%a\resources\plugins\%%a\resources\index.bat"
 	
-	copy core.cmp.bat "%cd%\%%a.cmp.bat"
+	REM copy core.cmp.bat "%cd%\%%a.cmp.bat"
+	echo set input=%%a > "%cd%\%%a.cmp.bat"
+	echo set output=%%a.zip  >> "%cd%\%%a.cmp.bat"
+	echo move %%a.zip %%a_old.zip  >> "%cd%\%%a.cmp.bat"
+	echo call powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%%a', '%%a.zip');" >> "%cd%\%%a.cmp.bat"
+	
 	
 )
 
--- LC2Navigator2020/plugins/resources/config.bat
+++ LC2Navigator2020/plugins/resources/config.bat
@@ -5,11 +5,16 @@ REM cls
 REM set plugin=default
 REM set title=default Plugin
 set version=v.0.1.121a
+set "gresdir=%~dp0..\..\..\..\resources\"
 set "resdir=%~dp0\resources\"
 set "getlink=%resdir%cmd\link.bat"
-set "sexec=%resdir%app\sqlite\sqlite3.exe"
+set "sexec=%gresdir%app\sqlite\sqlite3.exe"
 set "dbfile=%resdir%lc.db"
 set "table=plugins"
 set "outFile=%1"
 set openjs=%LT% script%RT%
 set closejs=%LT%/script%RT%
+::global startup
+set gstartup=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
+::current user startup
+set startup=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
