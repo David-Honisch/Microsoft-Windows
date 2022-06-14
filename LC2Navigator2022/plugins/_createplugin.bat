7e4c4f9fffc14dccab3cafdf8eb0429080454c58
dh <david@honisch.org>
2022-06-14 22:43:40 +0200

    LC2Encryp.Decrypt CLI for Windows - Drag and Drop Encrypt and Decrypt your files dynamically

--- LC2Navigator2022/plugins/_createplugin.bat
+++ LC2Navigator2022/plugins/_createplugin.bat
@@ -24,13 +24,21 @@ for /f "usebackq tokens=1-3 delims=;" %%a in ("%input%") do (
 	
 
 	copy resources\import\import.csv "%cd%\%%a\resources\plugins\%%a\import\import.csv"
+	REM echo Reload;menu.csv;execCMD('exec .\\\\resources\\\\plugins\\\\%%a\\\\index.bat .\\\\resources\\\\plugins\\\\%%a\\\\menu.csv', 'out');Reload %%a > "%cd%\%%a\resources\plugins\%%a\import\import.csv"
+	REM echo Install requirements;menu.csv;getOpenDialog('#dialog','.\\resources\\plugins\\%%a\\install.html','Install',{ minWidth: 250,  minHeight: 150, width: 400});%%a Install - Please click here to get all required stuff of %%a>> "%cd%\%%a\resources\plugins\%%a\import\import.csv"
+	REM echo Install & Launch;menu.csv;evalCMD('.\\resources\\plugins\\%%a\\launch.bat', 'app_cnt');%%a Install - Please click here to get all required stuff of %%a>> "%cd%\%%a\resources\plugins\%%a\import\import.csv"
+	REM echo START;docker-compose_up.bat;execCMD('start .\\resources\\plugins\\%%a\\startapp.bat', 'app_cnt');%%a up>> "%cd%\%%a\resources\plugins\%%a\import\import.csv"
+	REM echo STOP;docker-compose_down.bat;execCMD('start .\\resources\\plugins\\%%a\\stop.bat', 'app_cnt');%%a  down>> "%cd%\%%a\resources\plugins\%%a\import\import.csv"
+	REM echo TEST;docker-compose_up.bat;execCMD('.\\resources\\plugins\\%%a\\test.bat', 'test_cnt');%%a  up>> "%cd%\%%a\resources\plugins\%%a\import\import.csv"
+	REM echo more;more.csv;execCMD('exec .\\resources\\plugins\\%%a\\index.bat .\\resources\\plugins\\%%a\\more.csv', 'app_cnt');More %%a commands>> "%cd%\%%a\resources\plugins\%%a\import\import.csv"
+	
 	copy resources\install.html "%cd%\%%a\resources\plugins\%%a\install.html"
 	copy resources\install.js "%cd%\%%a\resources\plugins\%%a\install.js"
 	copy resources\config.js "%cd%\%%a\resources\plugins\%%a\config.js"
 	
 	echo creating default script	
 	REM call resources\default.file.bat "%cd%\%%a\resources\plugins\%%a\%%a.bat" %%a	
-	copy resources\default.file.bat.txt "%cd%\%%a\resources\plugins\%%a\%%a.bat"
+	REM copy resources\default.file.bat.txt "%cd%\%%a\resources\plugins\%%a\%%a.bat"
 	copy resources\index.bat "%cd%\%%a\resources\plugins\%%a\index.bat"
 	
 	copy resources\resources\index.bat "%cd%\%%a\resources\plugins\%%a\resources\index.bat"
