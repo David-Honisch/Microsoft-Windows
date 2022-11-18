eb95d1754377c3fb37a15480cf4f55c570920379
dh <david@honisch.org>
2022-11-19 00:29:54 +0100

    LC2Navigator2023 - crc sfvs

--- LC2Navigator2022/createinstaller.bat
+++ LC2Navigator2022/createinstaller.bat
@@ -1,5 +1,5 @@
 @echo off
 set input=install\install
 set output=install.zip
-C:\tools\nsis\makensisw.exe LC2Navigator2022.nsi
+C:\tools\nsis\makensisw.exe LC2Navigator2023.nsi
 REM zip -9 -m -o %output% %input%
