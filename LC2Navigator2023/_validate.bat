b147a9efdfabd769b2d9b884183b0f99ae42891e
dh <david@honisch.org>
2023-01-31 20:31:57 +0100

    crc32 checksum update

---
+++ LC2Navigator2023/_validate.bat
@@ -0,0 +1,17 @@
+@echo off
+set fileName=_validatefiles.csv
+set vfileName=validatedfiles.csv
+dir /b > %fileName%
+for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
+	LC2CRC32CLI.exe %%a > %~dp0sfv\%%~na.%%~xa-crc.sfv
+)
+dir /b plugins\*.zip > %fileName%
+for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
+	LC2CRC32CLI.exe plugins\%%a > %~dp0sfv\%%~na.%%~xa-crc.sfv
+)
+REM dir /b *.valid.* >%vfileName%
+REM for /f "usebackq tokens=1-4 delims=|" %%a in ("%vfileName%") do (
+	REM LC2CRC32CLI.exe %%a > .\sfv\%%~na.csv.csv
+REM )
+move *.sfv .\sfv\
+echo done.
