2aa86cb23e1553b64efad06f269bc90b25ea9866
dh <david@honisch.org>
2023-02-01 20:09:38 +0100

    crc checksum files

--- LC2Navigator2023/_validate.bat
+++ LC2Navigator2023/_validate.bat
@@ -3,11 +3,11 @@ set fileName=_validatefiles.csv
 set vfileName=validatedfiles.csv
 dir /b > %fileName%
 for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
-	LC2CRC32CLI.exe %%a > %~dp0sfv\%%~na.%%~xa-crc.sfv
+	LC2CRC32CLI.exe %%a > %~dp0sfv\%%~na%%~xa-crc.sfv
 )
 dir /b plugins\*.zip > %fileName%
 for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
-	LC2CRC32CLI.exe plugins\%%a > %~dp0sfv\%%~na.%%~xa-crc.sfv
+	LC2CRC32CLI.exe plugins\%%a > %~dp0sfv\%%~na%%~xa-crc.sfv
 )
 REM dir /b *.valid.* >%vfileName%
 REM for /f "usebackq tokens=1-4 delims=|" %%a in ("%vfileName%") do (
