828ad7e502106030c79fc09baa399d8f5fb0088e
dh <david@honisch.org>
2023-02-01 23:17:51 +0100

    LC2Navigator2023 new update including crc check validating plugin downloads

--- LC2CRC32/test.bat
+++ LC2CRC32/test.bat
@@ -1,4 +1,4 @@
 @echo off
 call LC2CRC32CLI.exe "C:\tools\LC2CRC\LC2CRC32CLI.exe" 1f943296 -validate
 call LC2CRC32CLI.exe "C:\tools\LC2CRC\LC2CRC.exe" 1f943296 -validate
-call LC2CRC32CLI.exe "C:\tools\LC2CRC\LC2CRC.exe" 1f943296 -validate
+call LC2CRC32CLI.exe "C:\tools\LC2CRC\LC2CRC.exe" 21f943296 -validate
