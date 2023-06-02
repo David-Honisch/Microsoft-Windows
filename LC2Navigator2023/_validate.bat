60fd6d0a044be87b2f376366157bb52d9c73189d
dh <david@honisch.org>
2023-06-02 22:43:03 +0200

    application item navigation fixed

--- LC2Navigator2023/_validate.bat
+++ LC2Navigator2023/_validate.bat
@@ -1,14 +1,20 @@
 @echo off
 set fileName=_validatefiles.csv
-set vfileName=validatedfiles.csv
+set vfileName=_v_validatedfiles.csv
+set delfileName=_delvalidatedfiles.csv
 dir /b > %fileName%
 for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
 	LC2CRC32CLI.exe %%a > %~dp0sfv\%%~na%%~xa-crc.sfv
+	move %~dp0sfv\%%~na%%~xa-crc.sfv .\sfv\
 )
 dir /b plugins\*.zip > %fileName%
 for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
 	LC2CRC32CLI.exe plugins\%%a > %~dp0sfv\%%~na%%~xa-crc.sfv
 	move %~dp0sfv\%%~na%%~xa-crc.sfv .\sfv\
+)
+dir /b /s *.sfv > %delfileName%
+for /f "usebackq tokens=1-4 delims=|" %%a in ("%delfileName%") do (
+	move %%a .\sfv\
 )
 REM dir /b *.valid.* >%vfileName%
 REM for /f "usebackq tokens=1-4 delims=|" %%a in ("%vfileName%") do (
