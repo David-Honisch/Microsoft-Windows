b48a3394a57bb5fb1f8f44f193ebe861aa147b83
dh <david@honisch.org>
2023-06-03 23:25:29 +0200

    CRC32 and more updated

--- LC2Navigator2023/_validate.bat
+++ LC2Navigator2023/_validate.bat
@@ -3,14 +3,14 @@ set fileName=_validatefiles.csv
 set vfileName=_v_validatedfiles.csv
 set delfileName=_delvalidatedfiles.csv
 dir /b > %fileName%
-for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
-	LC2CRC32CLI.exe %%a > %~dp0sfv\%%~na%%~xa-crc.sfv
-	move %~dp0sfv\%%~na%%~xa-crc.sfv .\sfv\
-)
+REM for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
+	REM LC2CRC32CLI.exe %%a > %~dp0sfv\%%~na%%~xa-crc.sfv
+	REM move %~dp0sfv\%%~na%%~xa-crc.sfv .\sfv\
+REM )
 dir /b plugins\*.zip > %fileName%
 for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
 	LC2CRC32CLI.exe plugins\%%a > %~dp0sfv\%%~na%%~xa-crc.sfv
-	move %~dp0sfv\%%~na%%~xa-crc.sfv .\sfv\
+	REM move %~dp0sfv\%%~na%%~xa-crc.sfv .\sfv\
 )
 dir /b /s *.sfv > %delfileName%
 for /f "usebackq tokens=1-4 delims=|" %%a in ("%delfileName%") do (
