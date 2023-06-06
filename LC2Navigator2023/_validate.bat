1f363d3c6140d7f92ad1fa4203b4371c2cce4401
dh <david@honisch.org>
2023-06-06 22:09:59 +0200

    all plugins sql script updated

--- LC2Navigator2023/_validate.bat
+++ LC2Navigator2023/_validate.bat
@@ -9,17 +9,18 @@ REM for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
 REM )
 dir /b plugins\*.zip > %fileName%
 for /f "usebackq tokens=1-4 delims=|" %%a in ("%fileName%") do (
+	LC2CRC32CLI.exe plugins\%%a > %~dp0%%~na%%~xa-crc.sfv
 	LC2CRC32CLI.exe plugins\%%a > %~dp0sfv\%%~na%%~xa-crc.sfv
 	REM move %~dp0sfv\%%~na%%~xa-crc.sfv .\sfv\
 )
-dir /b /s *.sfv > %delfileName%
-for /f "usebackq tokens=1-4 delims=|" %%a in ("%delfileName%") do (
-	move %%a .\sfv\
-)
+REM dir /b /s *.sfv > %delfileName%
+REM for /f "usebackq tokens=1-4 delims=|" %%a in ("%delfileName%") do (
+	REM move %%a .\sfv\
+REM )
 REM dir /b *.valid.* >%vfileName%
 REM for /f "usebackq tokens=1-4 delims=|" %%a in ("%vfileName%") do (
 	REM LC2CRC32CLI.exe %%a > .\sfv\%%~na.csv.csv
 REM )
-move *.sfv .\sfv\
-copy .\sfv\ .\plugins\
+REM move *.sfv .\sfv\
+REM copy .\sfv\ .\plugins\
 echo done.
