2598b9d19ba39a0f89ad53047672f3c2262b7684
dh <david@honisch.org>
2025-12-02 20:59:30 +0100

    all.zip download path  updated, plugins uploaded

---
+++ LC2Navigator2026/plugins/tools/lc2csvxml.cmp.bat
@@ -0,0 +1,8 @@
+set nppp=D:\tools\Notepad++\notepad++.exe
+set input=lc2csvxml
+set srcdir=..\..\pluginssrc\
+set input=%input%
+set output=%input%.zip
+move %input%.zip %input%_old.zip
+REM call %nppp% "..\pluginssrc\all\resources\sql\all.zip.sql"
+call powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%srcdir%%input%', '%input%.zip');"
